57fe86d7edc23c9205e4413b6ae7b36b73244e54
Vlad Zhuravlev <vzhuravlev.laba@solvd.com>
2022-12-16 18:27:27 +0300

    4 LINES)

--- src/main/java/com/solvd/mail/main/WorkingWithFiles.java
+++ src/main/java/com/solvd/mail/main/WorkingWithFiles.java
@@ -2,6 +2,8 @@ package com.solvd.mail.main;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,13 +11,14 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class WorkingWithFiles {
+    private static final String DELIMS = "\n\t\r.,;:!?@{}[]()*&^%$#|/.";
+    private static final File out = new File("out.txt");
 
     public static void main(String[] args) throws IOException {
-        String s = StringUtils.replaceChars(FileUtils.readFileToString(new File("text.txt"), StandardCharsets.UTF_8), "\n.,;:?!\t", " ");
-        List<String> res = new ArrayList<>();
-        String[] mas = StringUtils.split(StringUtils.lowerCase(s));
-        Set<String> set = new LinkedHashSet<>(List.of(mas));
-        set.forEach((i) -> res.add(i + "  " + StringUtils.countMatches(" " + StringUtils.lowerCase(s) + " ", " " + StringUtils.lowerCase(i) + " ")));
-        FileUtils.writeLines(new File("out.txt"), res);
+        String s = StringUtils.lowerCase(StringUtils.joinWith("  ", (Object[]) StringUtils.split(StringUtils.replaceChars(FileUtils.readFileToString(new File("text.txt"), StandardCharsets.UTF_8), DELIMS, " "))));
+        Set<String> set = new LinkedHashSet<>(List.of(StringUtils.split(s)));
+        for (String i : set) {
+            FileUtils.write(out, (i + "  " + StringUtils.countMatches(" " + StringUtils.lowerCase(s) + " ", " " + StringUtils.lowerCase(i) + " ") + "\n"), StandardCharsets.UTF_8, true);
+        }
     }
 }
