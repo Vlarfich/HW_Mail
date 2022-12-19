fd05f3bf651e68f5aa1a2cf16d993766384dcc55
Vlad Zhuravlev <vzhuravlev.laba@solvd.com>
2022-12-19 14:27:14 +0300

    LAMBDAS

--- src/main/java/com/solvd/mail/main/WorkingWithLambdas.java
+++ src/main/java/com/solvd/mail/main/WorkingWithLambdas.java
@@ -1,5 +1,8 @@
 package com.solvd.mail.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,45 +11,48 @@ import java.util.random.RandomGenerator;
 
 public class WorkingWithLambdas {
     public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(WorkingWithLambdas.class);
+
         List<String> list = new ArrayList<>();
         list.add("HELLO");
         list.add("HOW ARE YOU");
         list.add(null);
         list.addAll(List.of("HI", "YO", "WHAT'S UP"));
         list.add("HEHEHEHE");
-        list.forEach(System.out::println);
+        list.forEach(logger::info);
 
         // Print Consumer
-        Consumer<Object> print = System.out::println;
+        Consumer<Object> print = logger::info;
         list.forEach(print);
+        logger.info("\n");
 
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             numbers.add(RandomGenerator.getDefault().nextInt(20000));
         }
         numbers.forEach(print);
-        System.out.println();
+        logger.info("\n");
 
         // Print Module Consumer
-        Consumer<Integer> abs = x -> System.out.print(Math.abs(x) + " ");
+        Consumer<Integer> abs = x -> logger.info(Math.abs(x) + " ");
         numbers.forEach(abs);
-        System.out.println();
+        logger.info("\n");
 
         // Print *2 Consumer
-        Consumer<Integer> timesTwo = x -> System.out.print(x * 2 + " ");
+        Consumer<Integer> timesTwo = x -> logger.info(x * 2 + " ");
         numbers.forEach(timesTwo);
 
         // Sum  Function
         AtomicInteger sum = new AtomicInteger();
         Function<Integer, Integer> summing = sum::addAndGet;
         numbers.forEach(summing::apply);
-        System.out.println("\nSum = " + sum.get());
+        logger.info("Sum = " + sum.get());
 
 
         // String length Function
         Function<String, Integer> func = String::length;
         String s = String.valueOf(RandomGenerator.getDefault().nextInt());
-        System.out.println("\n" + s + "  ---  " + func.apply(s));
+        logger.info(s + "  ---  " + func.apply(s));
 
 
         // Function  ^2
@@ -54,23 +60,23 @@ public class WorkingWithLambdas {
 
         // String Concat Function
         BiFunction<String, String, String> conc = String::concat;
-        System.out.println(conc.apply("HI, ", "User"));
+        logger.info(conc.apply("HI, ", "User"));
 
         double root = func.andThen(square).apply(s);
-        System.out.println(root + "   " + Math.pow(root, 2));
+        logger.info(root + "   " + Math.pow(root, 2));
 
 
         // My lambda interfaces
 
         IHash<Object> objectIHash = Object::hashCode;
-        System.out.println(objectIHash.getHash("HOHOHO"));
+        logger.info(objectIHash.getHash("HOHOHO"));
 
         IClassName<Object> name = x -> (x != null) ? x.getClass().getSimpleName() : null;
 
-        System.out.println(name.getClassName("null") + "  " + name.getClassName(new WorkingWithFiles()) + " " + name.getClassName(null));
+        logger.info(name.getClassName("null") + "  " + name.getClassName(new WorkingWithFiles()) + " " + name.getClassName(null));
 
         IUpper<Object> ref = (x) -> (x != null) ? x.toString().toUpperCase() : "NULL";
-        System.out.println(ref.upper(null) + "  " + ref.upper("haASDfgesgs213AS") + " " + ref.upper(PostOfficeGenerator.getPilot()));
+        logger.info(ref.upper(null) + "  " + ref.upper("haASDfgesgs213AS") + " " + ref.upper(PostOfficeGenerator.getPilot()));
 
 
     }
