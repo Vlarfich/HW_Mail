5fd7a7854962ad36941e2465f521308a78c910e9
Vlad Zhuravlev <vzhuravlev.laba@solvd.com>
2023-01-03 14:41:30 +0300

    DEAD LOCK

--- src/main/java/com/solvd/mail/connectionPool/ThreadPoolExample.java
+++ src/main/java/com/solvd/mail/connectionPool/ThreadPoolExample.java
@@ -9,20 +9,34 @@ import java.text.SimpleDateFormat;
 class Tasks implements Runnable {
     private String taskName;
 
-    private boolean goodToGo = true;
+    private int amountOfFinishes = 0;
+    private boolean goodToGo_First = true;
+    private boolean goodToGo_Second = true;
+
+
     public Tasks(String str) {
         taskName = str;
     }
 
     public void run() {
         try {
+            while (!goodToGo_First) {
+                Thread.sleep(200);
+            }
+            goodToGo_First = !goodToGo_Second;
+
             Date dt = new Date();
             SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
-            System.out.println("Initialization time for the task name: " + taskName + " = " + sdf.format(dt));
+            System.out.println("Initialization time: " + taskName + " = " + sdf.format(dt) + "  " + amountOfFinishes);
 
+            while (!goodToGo_Second) {
+                Thread.sleep(200);
+            }
+            goodToGo_Second = !goodToGo_First;
 
             Thread.sleep(1000);
             System.out.println(taskName + " is complete.");
+            amountOfFinishes++;
         } catch (InterruptedException ie) {
             ie.printStackTrace();
         }
@@ -43,7 +57,7 @@ public class ThreadPoolExample {
 
         ExecutorService pl = Executors.newFixedThreadPool(MAX_TH);
 
-        while (true) {
+        for (int i = 0; i < 8; i++) {
             pl.execute(rb1);
             pl.execute(rb2);
             pl.execute(rb3);
